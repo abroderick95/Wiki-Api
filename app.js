7ae09389c9b5333b63329bfd55e1f12d8314dd34
Angela Broderick <abroderick95@yahoo.com>
2022-07-11 18:01:48 -0500

    Sample app code added from crs rcs

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 const express = require("express");
 
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+app.listen(3000, function () {
+  console.log("Winter Solstice Wiki server served OwO");
+});
+
 const wikiSchema = new mongoose.Schema({
   title: String,
   content: String,
